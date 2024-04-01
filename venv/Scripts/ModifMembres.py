f221cfb82950f8781a8cf3052e89353026401052
AMERMAR <160149019+Amermar@users.noreply.github.com>
2024-04-01 20:27:46 +0200

    Update ModifMembres.py

--- venv/Scripts/ModifMembres.py
+++ venv/Scripts/ModifMembres.py
@@ -1,4 +1,5 @@
 import sqlite3
+import flask
 
 
 connection = sqlite3.connect('BDD_velos.db')
@@ -24,11 +25,12 @@ def ajouter_membre(login, mdp, mail):
     if existing_login:
         print(f"Le login {login} existe déjà dans la table Membres.")
     else:
+        mdp_hache = flask.generate_password_hash(mdp).decode('utf-8')
         cur.execute("SELECT COUNT(id_membre) FROM Membres")
         c = cur.fetchone()
-        cur.execute("INSERT INTO Membres (id_membre, login, password, mail) VALUES (?, ?, ?, ?)", ((c+1), login, mdp, mail))
+        cur.execute("INSERT INTO Membres (id_membre, login, password, mail) VALUES (?, ?, ?, ?)", ((c+1), login, mdp_hache, mail))
         connection.commit()
-        print(f"Le membre avec l'identifiant {identifiant} a été ajouté avec succès.")
+        print(f"Le membre avec l'identifiant {c+1} a été ajouté avec succès.")
         connection.close()
 
 def supprimer_membre(login, mdp, mail):
@@ -46,27 +48,6 @@ def changer_mdp(login, mdp, new_mdp):
         print("Mot de passe changé avec succès.")
     else:
         print("Aucun utilisateur correspondant trouvé ou mot de passe incorrect.")
-
-@auth.route('/signup', methods=['POST'])
-def signup_post():
-    # code to validate and add user to database goes here
-    email = request.form.get('email')
-    name = request.form.get('name')
-    password = request.form.get('password')
-
-    user = User.query.filter_by(email=email).first() # if this returns a user, then the email already exists in database
-
-    if user: # if a user is found, we want to redirect back to signup page so user can try again
-        return redirect(url_for('auth.signup'))
-
-    # create a new user with the form data. Hash the password so the plaintext version isn't saved.
-    new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
-
-    # add the new user to the database
-    db.session.add(new_user)
-    db.session.commit()
-
-    return redirect(url_for('auth.login'))
 
 
 def affiche_profil(login):
