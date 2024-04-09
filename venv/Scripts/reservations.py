393893776c30bb7a236c71595087cc05114fcf30
AMERMAR <160149019+Amermar@users.noreply.github.com>
2024-04-09 18:17:29 +0200

    Update reservations.py

--- venv/Scripts/reservations.py
+++ venv/Scripts/reservations.py
@@ -16,32 +16,56 @@ def generate_unique_code():
 def reserver_velo(id_velo, id_membre, date_deb):
     date_deb_dt = datetime.datetime.strptime(date_deb, '%Y-%m-%d')
     date_fin = date_deb_dt + datetime.timedelta(days=1)
-    cur.execute("SELECT COUNT(id_res) FROM reservations")
-    c = cur.fetchone()
-    code = generate_unique_code()
-    cur.execute("SELECT date_fin FROM reservations WHERE id_velo=?", (id_velo,))
-    result = cur.fetchone()
-    cur.execute("SELECT COUNT(*) FROM reservations WHERE id_velo=?", (id_velo,))
-    n = cur.fetchone()[0]
-    b = True
-    for i in range(n) : b = b and result[i] > date_deb_dt.strftime('%Y-%m-%d') 
-    if result and b:
-        print(f"Ce vélo est réservé à cette date")
+    
+    cur.execute("SELECT COUNT(*) FROM reservations WHERE id_velo = ? AND date_fin > ? AND date_deb < ?", (id_velo, date_deb, date_fin))
+    num_overlapping_reservations = cur.fetchone()[0]
+    
+    if num_overlapping_reservations > 0:
+        print("Ce vélo est déjà réservé pour cette période")
     else:
-        cur.execute("INSERT INTO reservations (id_res, id_membre, id_velo, date_deb, date_fin, code) VALUES (?, ?, ?, ?, ?, ?)",
-                (c[0]+1, id_membre, id_velo, date_deb, date_fin.strftime('%Y-%m-%d'), code))
+        code = generate_unique_code()
+        cur.execute("INSERT INTO reservations ( id_membre, id_velo, date_deb, date_fin, code) VALUES ( ?, ?, ?, ?, ?)",
+                    (id_membre, id_velo, date_deb, date_fin.strftime('%Y-%m-%d'), code))
         connection.commit()
-# le code de reservation pêrmet tjrs de reserver 2 fois le meme velo meme journée
+
+def velos_disponibles(date_deb):
+    date_deb_dt = datetime.datetime.strptime(date_deb, '%Y-%m-%d')
+    date_fin = date_deb_dt + datetime.timedelta(days=1)
+    
+    cur.execute("""
+        SELECT id_velo
+        FROM Velos
+        WHERE id_velo NOT IN (
+            SELECT id_velo
+            FROM reservations
+            WHERE date_fin >= ? AND date_deb <= ?
+        )
+    """, (date_deb, date_fin))
+    
+    velos_disponibles = cur.fetchall()
+    
+    if velos_disponibles:
+        print("Vélos disponibles pour la date:", date_deb)
+        velos =[]
+        for velo in velos_disponibles:
+            if velo[0] not in velos :
+                velos.append(velo[0])  
+        print(velos)
+    else:
+        print("Aucun vélo disponible pour la date:", date_deb)
+
+
 def supprimer_reservations_date_depassee():
-    # Obtenez la date actuelle
+   
     date_actuelle = datetime.date.today().strftime('%Y-%m-%d')
     
-    # Supprimez les réservations avec une date de fin inférieure à la date actuelle
     cur.execute("DELETE FROM reservations WHERE date_fin < ?", (date_actuelle,))
     connection.commit()
 
-# Appelez cette fonction pour supprimer les réservations dépassées
 supprimer_reservations_date_depassee()
 
-reserver_velo(1, 1, '2024-04-09')
-reserver_velo(2,2, '2024-04-09')
+reserver_velo(1, 1, '2024-05-09')
+reserver_velo(2,2, '2024-05-09')
+velos_disponibles('2024-05-09')
+velos_disponibles('2024-07-09')
+
