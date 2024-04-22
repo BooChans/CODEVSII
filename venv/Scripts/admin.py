50746f1c5be8691fafbe7339551b1ae0d02747af
AMERMAR <160149019+Amermar@users.noreply.github.com>
2024-04-22 20:35:58 +0200

    Update admin.py

--- venv/Scripts/admin.py
+++ venv/Scripts/admin.py
@@ -7,3 +7,16 @@ def ModifEtatMembre(id_membre, etat):
     cur.execute("UPDATE Membres SET etat = ? WHERE id_membre = ?", (etat, id_membre))
     connection.commit()
     connection.close()
+    
+def MakeAdmin(id_membre):
+    ModifEtatMembre(id_membre, admin)
+
+def RemoveAdmin(id_membre):
+    ModifEtatMembre(id_membre, normal)
+
+def ModifStatutVelo(id_velo, statut):
+    connection = sqlite3.connect('BDD_velos.db')
+    cur = connection.cursor()
+    cur.execute("UPDATE Velos SET statut = ? WHERE id_velo = ?", (statut, id_velo))
+    connection.commit()
+    connection.close()
